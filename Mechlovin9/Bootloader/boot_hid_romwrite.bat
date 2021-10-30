8b5fe22871cd272ede92c0dee869325feb5c47e1
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-10-30 14:32:28 +0700

    update 30102021

--- Mechlovin9/Bootloader/boot_hid_romwrite.bat
+++ Mechlovin9/Bootloader/boot_hid_romwrite.bat
@@ -1,3 +1,3 @@
-avrdude -c stk500v2 -P com3 -p atmega32 -U hfuse:w:0xD0:m -U lfuse:w:0xCF:m
-avrdude -c stk500v2 -P com3 -p atmega32 -U flash:w:main.hex:i
+avrdude -c stk500v2 -P com5 -p atmega32 -U hfuse:w:0xD0:m -U lfuse:w:0xCF:m
+avrdude -c stk500v2 -P com5 -p atmega32 -U flash:w:main.hex:i
 pause;
