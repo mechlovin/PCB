7c56e78fcf53413ed8aaa3b72f07a25df738c599
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-11-08 17:13:56 +0700

    update 08112021
    Update Jay60 firmware, Adelais APM32F103, IF87.5 .step file

--- 60% PCB/Jay60/Rev1/Bootloader/boot_hid_romwrite.bat
+++ 60% PCB/Jay60/Rev1/Bootloader/boot_hid_romwrite.bat
@@ -1,3 +1,3 @@
-avrdude -c stk500v2 -P com3 -p atmega32 -U hfuse:w:0xC0:m -U lfuse:w:0x1F:m
-avrdude -c stk500v2 -P com3 -p atmega32 -U flash:w:Jay60-rev1-boot.hex:i
+avrdude -c stk500v2 -P com5 -p atmega32 -U hfuse:w:0xC0:m -U lfuse:w:0x1F:m
+avrdude -c stk500v2 -P com5 -p atmega32 -U flash:w:Jay60-rev1-boot.hex:i
 pause;
